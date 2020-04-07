47bd687fc8e8779f346392b323b8695d47804e63
Jan Roztropinski <jan.roztropinski@twino.pl>
2020-04-07 22:34:11 +0200

    game logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
-let playerMove = 'kamień';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'nożyce'
+} else if (randomNumber == 3) {
+    computerMove = 'papier'
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if( playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'nożyce'
+} else if (playerInput == '3') {
+    playerMove = 'papier'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if ((computerMove == 'kamień' && playerMove == 'papier') || (computerMove == 'nożyce' && playerMove == 'kamień') || (computerMove == 'papier' && playerMove == 'nożyce')) {
+    printMessage('Ty wygrywasz!');
+} else if ( (computerMove == 'papier' && playerMove == 'kamień') || (computerMove == 'kamień' && playerMove == 'nożyce') || (computerMove == 'nożyce' && playerMove == 'papier')) {
+    printMessage('Ja wygrywam!');
+} else if (computerMove === playerMove) {
+    printMessage('Remis!');
+}
+
